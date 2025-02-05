976718ceb8a6b59134042f694bb5bcaca720d7f1
xufeisofly <xufei1990@gmail.com>
2025-02-05 11:09:40 +0800

    fix

--- consensus/block_chain.go
+++ consensus/block_chain.go
@@ -18,6 +18,10 @@ const (
 type BlockChain interface {
 	// Store a block to the chain
 	Store(block *types.Block) error
+	// Store a block(committed) to database
+	Store2Db(block *types.Block) error
+	// Get a block by block hash
+	Get(blockHash Hash) *types.Block
 	// If has block
 	Has(blockhash Hash) bool
 	// If block and target share the same branch
@@ -31,9 +35,35 @@ type BlockChain interface {
 	GetAllVerified() []*types.Block
 	// Get all blocks ordered by view
 	GetOrderedAll() []*types.Block
+	// Get all children blocks of a block
+	GetRecursiveChildren(blockHash Hash) []*types.Block
 
 	// Get max view from the chain
 	GetMaxView() types.View
+	// Latest committed block
+	LatestCommittedBlock() *types.Block
+	// Latest locked block
+	LatestLockedBlock() *types.Block
+	// Set latest committed block
+	SetLatestCommittedBlock(block *types.Block)
+	// Set latest locked block
+	SetLatestLockedBlock(block *types.Block)
+
+	// Get QC of a block
+	GetQuorumCertOf(block *types.Block) *QuorumCert
+	// Set QC for a block
+	SetQuorumCertFor(block *types.Block, qc *QuorumCert)
+
+	// Is the chain valid
+	IsValid() bool
+	// Number of blocks in chain
+	Size() uint32
+	String() string
+
+	// Get a block referenced by qc
+	QuorumCertRef(block *types.Block) *types.Block
+	// Get a block referenced by parent hash
+	ParentRef(block *types.Block) *types.Block
 }
 
 type wrappedBlock struct {
